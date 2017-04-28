9799db0691f9729aea690c7c446cd540e56c02d2
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-28 15:14:22 +0100

    Adds test for plyers to view their own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,12 @@
 feature "Hit points" do
-  scenario "Viewing hit points" do
+  scenario "see player 2 hit points" do
     sign_in_and_play
-  expect(page).to have_content("Ben: 100HP")
-  end
+    expect(page).to have_content 'Chris: 100HP'
+    end
+
+
+  scenario "see player 1 hit points" do
+    sign_in_and_play
+    expect(page).to have_content 'Ben: 100HP'
+    end
 end
