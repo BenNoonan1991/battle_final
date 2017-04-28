c38cf25503283469ac8863c2f052ec7792d41226
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-28 11:46:19 +0100

    Adds switch_turns_spec and passes tests

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ class Game
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
-    @current_turn = player_1 
+    @current_turn = player_1
   end
 
   def player_1
@@ -19,6 +19,16 @@ class Game
 
   def attack(player)
     player.receive_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 
 end
