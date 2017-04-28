5cea03665f324fa58d571cbc1795c8da7a20ebe3
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-28 11:28:26 +0100

    Adds and passes tests for current turn method in game.lib

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,11 @@ require './lib/player.rb'
 
 class Game
 
+  attr_reader :current_turn
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
+    @current_turn = player_1 
   end
 
   def player_1
---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,18 @@
+require 'game'
+
+describe 'Switch turns' do
+  context 'show curent turn' do
+    scenario 'at the beginning of the game' do
+      sign_in_and_play
+      expect(page).to have_content "Ben's turn"
+    end
+  end
+
+    scenario 'after player 1 attacks' do
+      sign_in_and_play
+      click_button 'Attack'
+      click_link 'OK'
+      expect(page).not_to have_content "Ben's turn"
+      expect(page).to have_content "Chris's turn"
+    end
+end
