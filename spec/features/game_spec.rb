15bd7d27fc04f4504ea5d8c6bf9409d8ba77eca9
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-28 10:47:20 +0100

    Adds player 2 test to game_spec

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -9,10 +9,15 @@ describe Game do
 
   describe '#player_1' do
     it 'retrieves the first player' do
-      expect(game.player_1).to eq player_1  
+      expect(game.player_1).to eq player_1
     end
   end
 
+  describe '#player_2' do
+    it 'retrieves the first player' do
+      expect(game.player_2).to eq player_2   
+    end
+  end
 
   describe "#attack" do
     it "damages the player" do
